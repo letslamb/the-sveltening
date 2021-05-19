1047b6992c9a7032f0f8c2246c69588d09c68236
Michael Lamb <mflambjr@gmail.com>
2021-05-18 20:01:30 -0400

    import & configure vite-imagetools & mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,14 @@
+import { mdsvex } from "mdsvex";
+import { mdsvexConfig } from "./mdsvex.config.js";
 import path from 'path'
+import { imagetools } from 'vite-imagetools'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: [".svelte", ...mdsvexConfig.extensions],
+	preprocess: [
+		mdsvex(mdsvexConfig),
+	],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
@@ -10,7 +17,8 @@ const config = {
         alias: {
           $static: path.resolve('static')
         }
-      }
+      },
+      plugins: [imagetools({force: true})]
     }
 	}
 };
