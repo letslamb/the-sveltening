9346f408d5dbac715bc743271e0fe4e54afcd382
Michael Lamb <mflambjr@gmail.com>
2021-05-24 20:58:24 -0400

    add thumbnail image wrapper component

--- src/lib/components/images/ImageWrapper.svelte
+++ src/lib/components/images/ImageWrapper.svelte
@@ -1,15 +1,24 @@
 <script>
+  // This component wraps an image and allows it to become a thumbnail with a link to the full size 
+  // version of the image
 
+  // TODO: this should probably be moved to primitives for more general-purpose use. It's currently too tightly coupled with the Image components, but it doesn't have to be.
+
+  // currentSrc will be passed in from the child Image component, if thumbnail={true}
   export let currentSrc
+
+  // thumbnail is a boolean to be passed in if you want this component's behavior
   export let thumbnail
 
 
 </script>
 
+<!-- if we pass thumbnail={true} into the parent <ImageLoader> and down the image component hierarchy, render the child <Image> component as a thumbnail in the DOM -->
 {#if thumbnail}
-  <a sapper:noscroll href={currentSrc}>
+  <a sveltekit:noscroll href={currentSrc}>
     <slot></slot>
   </a>
+  <!-- otherwise render the child <Image> component unaltered through a bare slot -->
 {:else}
   <slot></slot>
 {/if}
