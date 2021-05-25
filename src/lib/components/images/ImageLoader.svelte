e7a434bc0b90a97b0bf92fd026f60e824954491a
Michael Lamb <mflambjr@gmail.com>
2021-05-24 20:57:55 -0400

    add dev-facing ImageLoader component

--- src/lib/components/images/ImageLoader.svelte
+++ src/lib/components/images/ImageLoader.svelte
@@ -1,14 +1,16 @@
 <script>
-  import IntersectionObserver from "./IntersectionObserver.svelte";
+  import IntersectionObserver from "../primitives/IntersectionObserver.svelte";
   import Image from "./Image.svelte";
 
   export let thumbnail;
-  // export let src;
   export let alt;
-  // export let width;
-  // export let height;
+  // 'src', 'width', and 'height' are for the normal <img> tag. 
+  export let src;
+  export let width;
+  export let height;
+  /* TODO: 'rounded' needs to be a custom property tht can be fed in by the dev. Maybe use a default value instead of making it a Svelte prop at all */
   export let rounded;
-  export let ariaLabel;
+  // 'processedImages' should be used when you have multiple procesed versions of an image to be served based on 
   export let processedImages;
 
 </script>
@@ -16,10 +18,10 @@
 <IntersectionObserver once={true} let:intersecting>
   {#if intersecting}
     <Image
+      {src}
+      {alt}
       {processedImages}
       {thumbnail}
-      {alt}
-      {ariaLabel}
       {rounded}
     />
   {/if}
