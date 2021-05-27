a64a8dc3640b754d4d93a40f780b547b40573c85
Michael Lamb <mflambjr@gmail.com>
2021-05-26 20:14:34 -0400

    document processedImages structure & remove rounded prop

--- src/lib/components/images/ImageLoader.svelte
+++ src/lib/components/images/ImageLoader.svelte
@@ -1,28 +1,42 @@
 <script>
-  import IntersectionObserver from "../primitives/IntersectionObserver.svelte";
+  import IntersectionObserver from "$lib/components/primitives/IntersectionObserver.svelte";
+  import ThumbnailWrapper from "$lib/components/primitives/ThumbnailWrapper.svelte";
   import Image from "./Image.svelte";
 
+  // 'thumbnail' is a boolean prop to initialize the ThumbnailWrapper component around your image
   export let thumbnail;
   export let alt;
   // 'src', 'width', and 'height' are for the normal <img> tag. 
   export let src;
   export let width;
   export let height;
-  /* TODO: 'rounded' needs to be a custom property tht can be fed in by the dev. Maybe use a default value instead of making it a Svelte prop at all */
-  export let rounded;
-  // 'processedImages' should be used when you have multiple procesed versions of an image to be served based on 
+  /**
+   * 'processedImages' should be used when you have multiple procesed versions of an image to be served based on viewport width. It can also handle multiple image formats
+   * @type {Object[]}
+   * @property {string} processedImages[].format - image format (webp, jpg, etc.)
+   * @property {number} processedImages[].width - image width
+   * @property {number} processedImages[].height - image height
+   * @property {string} processedImages[].src - relative path to the image from project root (starting with '/')
+   */
   export let processedImages;
 
 </script>
 
 <IntersectionObserver once={true} let:intersecting>
-  {#if intersecting}
+  {#if intersecting && thumbnail}
+    <ThumbnailWrapper let:href>
+      <Image
+        {src}
+        {alt}
+        {processedImages}
+        currentSrc={href}
+      />
+    </ThumbnailWrapper>
+  {:else if intersecting}
     <Image
       {src}
       {alt}
       {processedImages}
-      {thumbnail}
-      {rounded}
     />
   {/if}
 </IntersectionObserver>
