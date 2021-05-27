1021a2a392bcc316dead2c6d1ef4e6cdc1c7486b
Michael Lamb <mflambjr@gmail.com>
2021-05-26 23:35:50 -0400

    finish type annotations & comments for props

--- src/lib/components/images/ImageLoader.svelte
+++ src/lib/components/images/ImageLoader.svelte
@@ -3,12 +3,33 @@
   import ThumbnailWrapper from "$lib/components/primitives/ThumbnailWrapper.svelte";
   import Image from "./Image.svelte";
 
-  // 'thumbnail' is a boolean prop to initialize the ThumbnailWrapper component around your image
+  /**
+   * @type {boolean} - 'thumbnail=true' will wrap the image in an <a> tag
+  */
   export let thumbnail;
+  /**
+   * @type {string}
+  */
   export let alt;
+  /**
+   * @type {string}
+  */
   // 'src', 'width', and 'height' are for the normal <img> tag. 
   export let src;
+  // 'width' and 'height' props are for providing the image's dimensions for the browser to calculate its correct aspect ratio & avoid jank when the image loads. These props don't directly control the image's width and height in the DOM because the <Image> component has the following overriding CSS:
+  //    img {
+  //      width: 100%;
+  //      height: auto; 
+  //      ...
+  //    }
+  // Use a wrapper around <ImageLoader> (e.g. <figure>) for more direct control over an image's actual dimensions in the DOM.
+  /** 
+   * @type {string | number}
+  */
   export let width;
+  /** 
+   * @type {string | number}
+  */
   export let height;
   /**
    * 'processedImages' should be used when you have multiple processed versions of an image to be served based on viewport width. It can also handle multiple image formats
@@ -16,7 +37,7 @@
    * @property {string} processedImages[].format - image format (webp, jpg, etc.)
    * @property {number} processedImages[].width - image width
    * @property {number} processedImages[].height - image height
-   * @property {string} processedImages[].src - relative path to the image from project root (starting with '/')
+   * @property {string} processedImages[].src - relative image path from project root (starting with '/')
    */
   export let processedImages;
 
