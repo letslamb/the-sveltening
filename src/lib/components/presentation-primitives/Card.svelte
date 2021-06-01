0baf7e90a75f76ed2c9e83b2927d153103abc643
Michael Lamb <mflambjr@gmail.com>
2021-06-01 12:59:33 -0400

    new folder for visual primitive components

---
+++ src/lib/components/presentation-primitives/Card.svelte
@@ -0,0 +1,24 @@
+<script>
+
+
+</script>
+
+<section class="card-container">
+  <slot />
+</section>
+
+<style>
+
+  .card-container {
+    background: var(--card-bg-color, #fff);
+    display: flex;
+    padding: var(--card-padding, 1rem);
+    border: var(--card-border, 1px solid #aaa);
+    border-radius: var(--card-border-radius, 2px);
+    box-shadow: var(--card-box-shadow, 2px 2px 8px rgba(0, 0, 0, 0.1));
+  }
+
+  @media (min-width: 960px) {
+    
+  }
+</style>
