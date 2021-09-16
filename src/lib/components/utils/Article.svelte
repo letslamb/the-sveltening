80639d456b981cf6270aa46616aa28ed3858bea5
Michael Lamb <mflambjr@gmail.com>
2021-09-16 16:08:18 -0400

    add Article comp, same as Section comp

---
+++ src/lib/components/utils/Article.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { setContext, getContext } from 'svelte'
+  import { headingLevel } from '$lib/js/constants'
+  import { enhanceSection } from '$lib/js/actions'
+
+  /**
+   * @type {string}
+   * set an optional class name for the top-level element of this component to enable 
+   * scoped styling of each component instance from outside (in parent components or pages)
+  */
+  export let wrapperClass
+  let level
+
+  if (typeof getContext(headingLevel) === 'number') {
+    level = getContext(headingLevel) + 1
+    setContext(headingLevel, level)
+  } else {
+    level = 1
+    setContext(headingLevel, level)
+  }
+
+</script>
+
+<article
+  use:enhanceSection
+  class={wrapperClass}>
+  <slot />
+</article>
