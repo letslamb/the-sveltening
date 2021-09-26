009ede153830606f0310548f18cf11e342a7fed1
Michael Lamb <mflambjr@gmail.com>
2021-09-26 18:25:52 -0400

    delete dead code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,28 +1,11 @@
 <script>
-  // import TestImages from '$lib/components/combos/Demo.svelte'
-  import TestLayoutPrimitives from '$lib/components/primitives/Demo.svelte'
-  import ReelDemo from '$lib/components/primitives/ReelDemo.svelte'
-  import HeadingTagDemo from '$lib/components/primitives/HeadingTagDemo.svelte'
 
-  import TestModal from '$lib/components/combos/modal/ModalWithButton.svelte'
-  import TestSecondModal from '$lib/components/combos/modal/Modal.svelte'
-  import ToggleSection from '$lib/components/utils/ToggleSection.svelte'
-  import Section from '$lib/components/utils/Section.svelte'
+  import TestLayoutPrimitives from '$lib/components/demos/Demo.svelte'
 
 </script>
 
 
 <TestLayoutPrimitives />
 
-<!-- <Section>
-  <ToggleSection>
-    <p>Here's some content</p>
-  </ToggleSection>
-</Section> -->
-
-
-<!-- <HeadingTagDemo /> -->
-
-<!-- <ReelDemo /> -->
 
 
