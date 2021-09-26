09598ff8b2eaabe87f868675946d5c51c2bb0d93
Michael Lamb <mflambjr@gmail.com>
2021-09-26 18:29:05 -0400

    rename everylayout-inspired comps folder

---
+++ src/lib/components/layout-primitives/HeadingTagDemo.svelte
@@ -0,0 +1,8 @@
+<script>
+  import HeadingTag from '$lib/components/utils/HeadingTag.svelte'
+  import Section from '$lib/components/utils/Section.svelte'
+</script>
+
+<Section>
+  <HeadingTag message={"testing"} />
+</Section>
