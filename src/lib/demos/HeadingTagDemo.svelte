4af955fe7ff11c15a12016ec5ea923c5469f32d4
Michael Lamb <mflambjr@gmail.com>
2021-10-08 23:01:32 -0400

    remove component subfolder, reorg file structure

---
+++ src/lib/demos/HeadingTagDemo.svelte
@@ -0,0 +1,8 @@
+<script>
+  import HeadingTag from '$lib/toolbox/heading-tag/HeadingTag.svelte'
+  import Section from '$lib/toolbox/section/Section.svelte'
+</script>
+
+<Section>
+  <HeadingTag message={"testing"} />
+</Section>
---
+++ src/lib/toolbox/section/Section.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { setContext, getContext } from 'svelte'
+  import { headingLevel } from '$lib/js/constants'
+  import { enhanceSection } from '$lib/js/actions.js'
+
+  /**
+   * @type {string}
+   * set an optional class name for the top-level element of this component to enable 
+   * scoped styling of each component instance from outside (in parent components or pages)
+  */
+  export let wrapperClass = ''
+  
+  let level
+
+  if (typeof getContext(headingLevel) === 'number') {
+    level = getContext(headingLevel) + 1
+    setContext(headingLevel, level)
+  } else {
+    level = 1
+    setContext(headingLevel, level)
+  }
+
+</script>
+
+<section
+  use:enhanceSection
+  class={wrapperClass}>
+  <slot />
+</section>
